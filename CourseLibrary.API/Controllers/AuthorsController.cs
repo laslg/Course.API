494dd9ff30396d09524b49e63370e598ff9a0d0b
llg <lasselg@gmail.com>
2019-12-02 20:40:35 +0100

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ??
+                   throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authors = courseLibraryRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = courseLibraryRepository.GetAuthor(authorId);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(author);
+        }
+    }
+}
