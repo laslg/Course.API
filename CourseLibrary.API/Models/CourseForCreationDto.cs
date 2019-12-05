8339e62c9ecc5f5368e5232f834d703c34a6ddc0
llg <lasselg@gmail.com>
2019-12-05 21:48:12 +0100

    Validation

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must != Desc")]
+    public class CourseForCreationDto// : IValidatableObject
     {
+        [Required(ErrorMessage = "Title needed")]
+        [MaxLength(100, ErrorMessage = "Max 100 chars")]
         public string Title { get; set; }
+
+        [MaxLength(1500, ErrorMessage = "Max 1500 chars for desc")]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("Error: Title == Description.", new[] { nameof(CourseForCreationDto) });
+        //    }
+        //}
     }
 }
